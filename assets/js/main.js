de3b35302052edfdafa1718482042655c47b7d1c
potopus <Paveldelo@yandex.ru>
2024-01-11 12:31:01 +0300

    refactor: fix bugs: change class

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ if (tableNames) {
                 });
                 tableBodies.forEach(element => {
                     element.classList.remove("active");
-                    if(element.classList.contains(tableName.id)){
+                    if (element.classList.contains(tableName.id)) {
                         element.classList.add("active");
                     }
                 });
@@ -26,15 +26,18 @@ let burgerIcon = document.querySelector("#check-menu");
 let menu = document.querySelector(".header-menu");
 let lebelBurger = document.querySelector(".label-burger");
 
-burgerIcon.addEventListener('change', ()=>{
+burgerIcon.addEventListener('change', () => {
 
-    if (burgerIcon.checked){
+    if (burgerIcon.checked) {
         menu.classList.add("header-menu__active");
         document.body.classList.add("lock");
-    }else{
+        document.documentElement.classList.add("lock");
+    } else {
         menu.classList.remove("header-menu__active");
         document.body.classList.remove("lock");
-        
+        document.documentElement.classList.remove("lock");
+
+
     }
 });
 
@@ -42,26 +45,27 @@ menu.addEventListener("click", function () {
     if (burgerIcon.checked) {
         menu.classList.remove("header-menu__active");
         document.body.classList.remove("lock");
+        document.documentElement.classList.remove("lock");
         burgerIcon.checked = false;
     }
 });
 
-var firstElements = document.querySelectorAll('.on_mobile .ah2');
+var firstElements = document.querySelectorAll('.mobile-table .ah2');
 
 // Добавляем слушатель события click для каждого элемента "first"
 firstElements.forEach(function (firstElement) {
-  firstElement.addEventListener('click', function () {
-    // Находим следующий элемент с классом "second"
-    var nextSecondElement = firstElement.nextElementSibling;
-    console.log("click");
-    console.dir(nextSecondElement);
+    firstElement.addEventListener('click', function () {
+        // Находим следующий элемент с классом "second"
+        var nextSecondElement = firstElement.nextElementSibling;
+        console.log("click");
+        console.dir(nextSecondElement);
 
-    // Проверяем, существует ли следующий элемент с классом "second"
-    if (nextSecondElement && nextSecondElement.classList.contains('hide')) {
-      // Добавляем класс "active" к следующему элементу "second"
-console.log("exist");
-      firstElement.classList.toggle('active');
-      nextSecondElement.classList.toggle('active');
-    }
-  });
+        // Проверяем, существует ли следующий элемент с классом "second"
+        if (nextSecondElement && nextSecondElement.classList.contains('hide')) {
+            // Добавляем класс "active" к следующему элементу "second"
+            console.log("exist");
+            firstElement.classList.toggle('active');
+            nextSecondElement.classList.toggle('active');
+        }
+    });
 });
